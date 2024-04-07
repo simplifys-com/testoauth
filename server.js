4fbab7cacfb5715729029ea51ef81120f3381bfd
Awadesh Madhogaria <awadesh@simplifys.com>
2024-04-07 14:46:45 +0800

    Update

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+const __dirname = path.resolve();
+
 import { googleOauth, googleCallback } from "./controllers/auth.controller.js";
 
 const app = express();
@@ -13,6 +16,11 @@ app.get("/", (req, res) => {
 
 app.get("/api/auth/google", googleOauth);
 app.get("/auth/google/callback", googleCallback);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
